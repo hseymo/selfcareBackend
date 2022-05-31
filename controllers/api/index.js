2e5e6741a2410fed1a2dd8b63a07fc8651869889
Haley Seymour <haleycseymour@comcast.net>
2022-05-31 11:08:43 -0700

    index model with relations

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const userRoutes = require("./userController")
 
+const userRoutes = require("./userController")
 router.use("/users",userRoutes)
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -6,6 +6,14 @@ class User extends Model {}
 
 User.init({
     // add properites here, ex:
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull:false,
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull:false,
+    }, 
     email: {
          type: DataTypes.STRING,
          unique:true,
@@ -21,9 +29,12 @@ User.init({
             len:[8]
         }
     }
-
 },{
     sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user', 
     hooks:{
         beforeCreate: newUser=>{
             newUser.password = bcrypt.hashSync(newUser.password,4)
